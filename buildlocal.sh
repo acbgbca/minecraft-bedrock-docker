6fc632e86b2c1a9e08c7eef3cfe3cccc16a31424
Andrew <60839662+acbgbca@users.noreply.github.com>
2022-06-05 16:05:09 +1000

    Resolves #12 Refactored Dockerfile to be multi-stage

--- buildlocal.sh
+++ buildlocal.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+target=${args[0]:-app}
+
 export URL=`curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" https://www.minecraft.net/en-us/download/server/bedrock|grep -o "https://minecraft.azureedge.net/bin-linux/bedrock-server-.*.zip"`
 export VERSION=`echo $URL|grep -o "[0-9.]*[0-9]"`
+export VERSION_MAJOR=`echo $VERSION|grep -o "[0-9]*\.[0-9]*"|head -1`
 
 if [ -z "$(ls bedrock-server.zip)" ]; then
     curl $URL --output ./bedrock-server.zip
@@ -9,5 +12,7 @@ fi
 
 echo "URL: $URL"
 echo "VERSION: $VERSION"
-
-docker build -f Dockerfile-local -t minecraft-bedrock:alpha . --build-arg URL=$URL --build-arg VERSION=$VERSION
+echo "MAJOR VERSION: $VERSION_MAJOR"
+echo "Build Target: $target"
+export DOCKER_BUILDKIT=1
+docker build --target $target -t minecraft-bedrock:alpha . --build-arg URL=./bedrock-server.zip --build-arg VERSION=$VERSION
