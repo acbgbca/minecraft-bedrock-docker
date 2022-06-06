d2ab8f9986144cbc2d1767a0731e9833165735b8
Andrew <60839662+acbgbca@users.noreply.github.com>
2022-06-06 19:58:43 +1000

    Fixed build local script to correctly read script arguments

--- buildlocal.sh
+++ buildlocal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target=${args[0]:-app}
+target=${1:-app}
 
 export URL=`curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" https://www.minecraft.net/en-us/download/server/bedrock|grep -o "https://minecraft.azureedge.net/bin-linux/bedrock-server-.*.zip"`
 export VERSION=`echo $URL|grep -o "[0-9.]*[0-9]"`
