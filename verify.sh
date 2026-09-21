14e787d6abfc1c4fa8b3c9befa35e4c59fdb0385
acbgbca <60839662+acbgbca@users.noreply.github.com>
2026-09-21 10:56:27 +1000

    Clean up debug scripts

--- verify.sh
+++ verify.sh
@@ -3,8 +3,6 @@
 # Start server in the background
 ./start.sh&
 
-ps
-
 # Wait up to 60 seconds for the server to start
 SECONDS=0
 until ss -tulnp | grep 19132
@@ -15,9 +13,14 @@ do
      echo "Giving up..."
      exit 1
   fi
-
-  ss -tulnp
-  ps
+  
   echo "Bedrock server is not up yet. Waiting..."
+
+  echo "\n Running processes:"
+  ps
+
+  echo "\n Open Ports:"
+  ss -tulnp
+
   sleep 5
 done
